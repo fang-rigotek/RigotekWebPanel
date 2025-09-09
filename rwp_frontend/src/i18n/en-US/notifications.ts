d3d66459eacc6181125289b9cff0565734493ae8
Fang&Rigotek <fang@rigotek.com>
2025-09-09 16:30:44 +0200

    更新文本

--- rwp_frontend/src/i18n/en-US/notifications.ts
+++ rwp_frontend/src/i18n/en-US/notifications.ts
@@ -3,7 +3,7 @@
 export default {
   browserOutdated: 'Browser is outdated',
 
-  // jsReq:'Rigotek Web Panel requires JavaScript enabled to run',
-  // ecma2022Req:'Rigotek Web Panel requires a browser that supports ECMAScript 2022 to run (e.g. Chrome 96+ or Safari 16.4+)',
-  wasm2Req: 'Rigotek Web Panel requires a browser that partially supports WebAssembly 2.0 to run (e.g. Chrome 96+ or Safari 16.4+)',
+  // jsReq:'Rigotek Panel requires JavaScript enabled to run',
+  // ecma2022Req:'Rigotek Panel requires a browser that supports ECMAScript 2022 to run (e.g. Chrome 96+ or Safari 16.4+)',
+  wasm2Req: 'Rigotek Panel requires a browser that partially supports WebAssembly 2.0 to run (e.g. Chrome 96+ or Safari 16.4+)',
 };
